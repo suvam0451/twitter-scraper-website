5c1f30f07fd774634f4cccb47a65ca0a33b3836c
Debashish Patra <suvam0451@protonmail.com>
2022-08-15 23:28:19 +0530

    chore: try a counter component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,19 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react'
 import BaseLayout from '../layouts/BaseLayout'
 import styles from '../styles/Home.module.css'
+
+export function Segs() {
+  const [clicked, setClicked] = useState(3);
+  return (
+    <div className="App">
+      <button onClick={() => setClicked((clicked - 1) * 2)}>Henlo</button>
+      <h1>{clicked}</h1>
+    </div>
+  );
+}
 
 const Home: NextPage = () => {
   return (
@@ -18,7 +29,7 @@ const Home: NextPage = () => {
           <h1 className={styles.title}>
             Welcome to <a href="https://nextjs.org">Next.js!</a>
           </h1>
-
+      <Segs/>
           <p className={styles.description}>
             Get started by editing{' '}
             <code className={styles.code}>pages/index.tsx</code>
--- pages/scraper.tsx
+++ pages/scraper.tsx
@@ -22,7 +22,6 @@ const TwitterScraper: NextPage = () => {
       </Provider>
     </BaseLayout>
     </>
-
   )
 }
 
