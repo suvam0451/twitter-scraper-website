b0e5319b0510e21ae241ea181c5a92fedb6ede43
Debashish Patra <suvam0451@gmail.com>
2022-10-27 12:43:55 +0530

    chore: config file

--- next.config.js
+++ next.config.js
@@ -5,11 +5,13 @@ const withPWA = require('next-pwa')({
 
 const nextConfig = withPWA({
   webpack: (config) => {
-    config.resolve.fallback = { ...config.resolve.fallback, fs: false };
-    return config;
+    config.resolve.fallback = { ...config.resolve.fallback, fs: false }
+    return config
   },
   env: {
     BACKEND_HOST: process.env.BACKEND_HOST,
     BACKEND_PORT: process.env.BACKEND_PORT
   }
-}
+})
+
+module.exports = nextConfig
